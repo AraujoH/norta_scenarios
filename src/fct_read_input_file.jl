26f5e727d28e0d3f6003503036867db8ff0b1824
Hugo de Araujo <santarem.hugo@gmail.com>
2026-01-27 11:56:50 -0500

    Update input-file reading function

--- src/fct_read_input_file.jl
+++ src/fct_read_input_file.jl
@@ -1,3 +1,18 @@
+function parse_intraday_hours(s::AbstractString)
+    s = strip(uppercase(s))
+
+    if s == "N"
+        return Int[]
+    end
+
+    try
+        return parse.(Int, split(s, ","))
+    catch
+        error("Invalid input. Use a comma-separated list of integers or 'N'.")
+    end
+end
+
+
 function read_input_file(filepath)
     f = open(filepath, "r")
 
@@ -22,26 +37,47 @@ function read_input_file(filepath)
     close(f)
 
     # Return lines
-    return(strip(lowercase(lines[1])), 
-           parse(Int, lines[2]), 
-           parse(Int, lines[3]), 
-           parse(Int, lines[4]), 
-           parse(Int, lines[5]),
-           parse(Int, lines[6]),
-           parse(Int, lines[7]),
-           parse(Int, lines[8]),
-           parse(Int, lines[9]),
-           parse.(Int, split(lines[10],",")),
-           strip(lines[11]),
-           strip(lines[12]),
-           strip(lines[13]),
-           strip(lines[14]),
-           strip(lines[15]),
-           strip(lines[16]),
-           strip(lines[17]),
-           strip(lines[18]),
-           strip(lines[19]),
-           parse(Int, lines[20]))
+    return (
+        # Data type - load, solar, wind
+        strip(lowercase(lines[1])),
+        # Scenario length
+        parse(Int, lines[2]),
+        # Scenario paths/number of scenarios
+        parse(Int, lines[3]),
+        # 
+        parse(Int, lines[4]),
+        #
+        parse(Int, lines[5]),
+        #
+        parse(Int, lines[6]),
+        #
+        parse(Int, lines[7]),
+        #
+        parse(Int, lines[8]),
+        #
+        parse(Int, lines[9]),
+        #
+        parse_intraday_hours(lines[10]),
+        # 
+        strip(lines[11]),
+        #
+        strip(lines[12]),
+        #
+        strip(lines[13]),
+        #
+        strip(lines[14]),
+        #
+        strip(lines[15]),
+        #
+        strip(lines[16]),
+        #
+        strip(lines[17]),
+        #
+        strip(lines[18]),
+        #
+        strip(lines[19]),
+        #
+        parse(Int, lines[20]))
 end
 
 

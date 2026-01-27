b063428d3e460f0c9b10fddf60d3a6ce2e8f8e15
Hugo de Araujo <santarem.hugo@gmail.com>
2026-01-27 13:04:04 -0500

    Create write_scenarios_to_file

--- src/fct_write_scenarios.jl
+++ src/fct_write_scenarios.jl
@@ -18,3 +18,34 @@ function write_scenarios(x::AbstractMatrix, type::String)
     filename = "scenarios_jul_18_48_" * type * ".csv"
     CSV.write(joinpath(filepath, filename), Tables.table(x), header=false)
 end
+
+function write_scenarios_to_file(
+    x::Array{Float64, 3},
+    scenario_day::Int,
+    scenario_month::Int,
+    scenario_year::Int,
+    data_type::String;
+    filepath::Union{Nothing,String}=nothing,
+    is_idm::Bool=false
+)
+
+    # Reshape x to 2D
+    reshaped_x = reduce(vcat, eachslice(x, dims=1))
+
+    # Set output directory
+    filepath = filepath === nothing ? mkpath(datadir("exp_pro")) : mkpath(filepath)
+
+    # Build filename
+    idm_tag = is_idm ? "_IDM" : ""
+    filename =
+        data_type * "_" *
+        "scenarios_" *
+        idm_tag *
+        string(scenario_year) * "_" *
+        lpad(scenario_month, 2, '0') * "_" *
+        lpad(scenario_day, 2, '0') * "_" * 
+        ".csv"
+
+    # Write to CSV
+    CSV.write(joinpath(filepath, filename), Tables.table(reshaped_x), header=false)
+end
